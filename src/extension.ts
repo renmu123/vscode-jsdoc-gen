3fad50c2748171cf893dc24fa3f5b28926d7d75e
fyx <fyx@030buy.jp>
2022-03-07 18:49:45 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode'
+import { genJSDoc } from './main'
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    'extension.genJSDoc',
+    genJSDoc
+  )
+
+  context.subscriptions.push(disposable)
+}
+
+export function deactivate() {}
